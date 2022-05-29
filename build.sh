825c73ada5746f5a99762e2632efabe19f931e88
Kokoshnikov Alexey <alexeikokoshnikov@gmail.com>
2022-05-29 18:02:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-TOOLCHAIN=C:\\Users\\k00535555\\Downloads\\android-ndk-r21d-windows-x86_64\\android-ndk-r21d
-NINJA=C:\\Users\\k00535555\\Downloads\\ninja-win\\ninja
+TOOLCHAIN=<path to NDK>/android-ndk-r21d
+NINJA=<path to ninja>/ninja
 cmake -GNinja -DCMAKE_MAKE_PROGRAM=$NINJA --DCMAKE_INSTALL_PREFIX:PATH=C:\\Repositories\\CrossFitTimer -DCMAKE_TOOLCHAIN_FILE=$TOOLCHAIN/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-24 -DANDROID_STL=c++_static .
 cmake --build . --target install
 cmake --build . --target clean
