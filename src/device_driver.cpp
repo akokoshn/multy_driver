c9bb5d1d9da4e7d3aae73fca3117d678738099ba
Kokoshnikov Alexey <alexeikokoshnikov@gmail.com>
2022-05-29 18:00:15 +0300

    Create device_driver.cpp

---
+++ src/device_driver.cpp
@@ -0,0 +1,21 @@
+//
+// Created by akokoshn on 5/28/2022.
+//
+
+#include "device_driver.h"
+
+std::vector<DeviceDescriptor> getDevices()
+{
+    std::vector<DeviceDescriptor> list;
+    return list;
+}
+
+int sparing(const std::string &name)
+{
+    return 0;
+}
+
+int getHR()
+{
+    return 0;
+}
